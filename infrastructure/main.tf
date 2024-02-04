ebd34d3b285d9db864aab79b20316361e30ad29f
Martin Chamambo <chamambom@gmail.com>
2024-02-04 13:27:18 +1300

    Changes to be committed:

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,7 +41,12 @@ resource "aws_flow_log" "example" {
 }
 
 resource "aws_s3_bucket" "example" {
-  #checkov:skip=CKV_AWS_18, CKV2_AWS_62, CKV2_AWS_6, CKV2_AWS_61, CKV_AWS_21, CKV_AWS_145:
+  #checkov:skip=CKV_AWS_18:
+  #checkov:skip=CKV2_AWS_62:
+  #checkov:skip=CKV2_AWS_6:
+  #checkov:skip=CKV2_AWS_61:
+  #checkov:skip=CKV_AWS_21:
+  #checkov:skip=CKV_AWS_145:  
   bucket = "tf-flow-logs"
 }
 
