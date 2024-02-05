35896190e648d3f1fc38721895f546490408130a
Martin Chamambo <chamambom@gmail.com>
2024-02-05 23:01:51 +1300

    Changes to be committed:

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,6 +20,7 @@ locals {
 # VPC
 resource "aws_vpc" "vpc" {
   #checkov:skip=CKV2_AWS_12:
+  #checkov:skip=CKV2_AWS_11:
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
