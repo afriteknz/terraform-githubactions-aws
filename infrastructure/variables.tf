33b0de4fcdc22f15ab13db18560f571a2ff4cc4a
Martin Chamambo <chamambom@gmail.com>
2024-02-15 09:59:31 +1300

    Changes to be committed:

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,18 +7,18 @@ variable "environment" {
 }
 
 variable "vpc_cidr" {
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/23"
   description = "CIDR block of the vpc"
 }
 
 variable "public_subnets_cidr" {
   type        = list(any)
-  default     = ["10.0.0.0/20", "10.0.128.0/20"]
+  default     = ["10.0.0.0/24", "10.0.128.0/24"]
   description = "CIDR block for Public Subnet"
 }
 
 variable "private_subnets_cidr" {
   type        = list(any)
-  default     = ["10.0.16.0/20", "10.0.144.0/20"]
+  default     = ["10.0.16.0/24", "10.0.144.0/24"]
   description = "CIDR block for Private Subnet"
 }
