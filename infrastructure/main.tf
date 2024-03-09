f92fd0d45408f25fb823255d4cd54a0a3e3ed809
Martin Chamambo <chamambom@gmail.com>
2024-03-10 12:59:32 +1300

    Changes to be committed:

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -70,7 +70,7 @@ resource "aws_eip" "nat_eip" {
   depends_on = [aws_internet_gateway.ig]
 }
 
-#NAT Gateway
+##NAT Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
